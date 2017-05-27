6fcf9010a9aa0db6a1c6b31a4462d51552ca245e
nattop <nathan@windisch.co.uk>
2017-05-27 10:30:44 +0100

    added a new 404 page

---
+++ new404/404/js/demo.js
@@ -0,0 +1,36 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa',
+    density: 30000,
+    parallax: true,
+    curvedLines: false,
+    particleRadius: 1,
+    lineWidth: 0.5,
+    minSpeedX: 1,
+    minSpeedY: 1,
+    directionX: 'center',
+    directionY: 'center'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
